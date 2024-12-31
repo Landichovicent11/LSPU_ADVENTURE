638d8d323e0d7852ff80e3910a36a210af215261
@Landichovicent11 <0323-4415@lspu.edu.ph>
2024-12-31 10:47:48 +0800

    Upadate Button Function P1

--- LSPU_ADVENTURE/Map.cs
+++ LSPU_ADVENTURE/Map.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.
 
 namespace LSPU_ADVENTURE
 {
@@ -26,7 +27,39 @@ namespace LSPU_ADVENTURE
 
         private void LecCSS_Click(object sender, EventArgs e)
         {
+            Random rnd = new Random();
+            HashSet<int> UniqueRnd = new HashSet<int>();//Ensure rnd Uniqueness
+            List<string> Games = new List<string> {"ArtApp","Discrete","IM"};
+            bool GenerateGame = true;
 
+            do
+            {
+                int RandomGame = rnd.Next(0, 3);
+                UniqueRnd.Add(RandomGame);
+
+                if (GenerateGame == true) 
+                {
+                    foreach (int GameGenerated in UniqueRnd)
+                    {
+                        String Playing = Games[GameGenerated];
+                        switch (Playing) {
+                            case "ArtApp":
+                                //process artapp put mo game mo ina mo dito mo i loload yun form ng game para mag start
+                                break;
+                            case "Discrete":
+                                //process Discrete
+                                break;
+                            case "IM":
+                                //process IM
+                                break;
+                        }
+                        
+                    }
+                    
+                }
+            } while (UniqueRnd.Count < 3);
+
+            
         }
 
         private void PE_Click(object sender, EventArgs e)
